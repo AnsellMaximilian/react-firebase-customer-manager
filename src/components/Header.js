da3c72e71046e9d7995526ebd5592f703d4608ad
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-12-25 20:14:18 +0700

    fix header container

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import logo from "../assets/images/logo.svg";
 
 export const Header = () => {
   return (
-    <header className="px-4 py-2 shadow-md container mx-auto">
-      <nav className="flex">
+    <header className="py-2 shadow-md">
+      <nav className="flex container mx-auto px-4">
         <img src={logo} alt="logo" width={100} />
         <button className="ml-auto">Logout</button>
       </nav>
