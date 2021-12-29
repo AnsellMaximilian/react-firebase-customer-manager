35d171c557ce644c673aaadf47ac35daae44c6eb
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-12-29 09:53:28 +0700

    more links on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,14 +14,30 @@ export const Header = () => {
   };
 
   return (
-    <header className="py-2 shadow-md">
-      <nav className="flex container mx-auto px-4">
-        <Link to="/">
-          <img src={logo} alt="logo" width={100} />
-        </Link>
-        <button className="ml-auto" onClick={handleSignOut}>
-          Logout
-        </button>
+    <header className="shadow-md">
+      <nav className="border-b border-grey-400 py-2">
+        <div className="container mx-auto flex px-4 ">
+          <Link to="/">
+            <img src={logo} alt="logo" width={100} />
+          </Link>
+          <button className="ml-auto" onClick={handleSignOut}>
+            Logout
+          </button>
+        </div>
+      </nav>
+      <nav className="container mx-auto px-4">
+        <ul className="flex gap-4">
+          <li className="transition-all border-b-2 font-bold text-gray-500 hover:text-black border-white hover:border-green-700">
+            <Link to="/customers" className="px-3 py-1 block ">
+              Customers
+            </Link>
+          </li>
+          <li className="transition-all hover:border-b-2 font-bold text-gray-500 hover:text-black border-green-700">
+            <Link to="/customers" className="px-3 py-1 block ">
+              Customers
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
