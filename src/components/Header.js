60642e6e476f59e580e4ba0afaf1b57b18f1f173
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-12-29 10:16:33 +0700

    add page links and styled them

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from "../assets/images/logo.svg";
 import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase";
 import { SnackbarContext } from "../contexts/SnackbarContext";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const Header = () => {
   const { openSnackbar } = useContext(SnackbarContext);
@@ -27,18 +27,31 @@ export const Header = () => {
       </nav>
       <nav className="container mx-auto px-4">
         <ul className="flex gap-4">
-          <li className="transition-all border-b-2 font-bold text-gray-500 hover:text-black border-white hover:border-green-700">
-            <Link to="/customers" className="px-3 py-1 block ">
-              Customers
-            </Link>
+          <li className="">
+            <PageLink to="/customers">Customers</PageLink>
           </li>
-          <li className="transition-all hover:border-b-2 font-bold text-gray-500 hover:text-black border-green-700">
-            <Link to="/customers" className="px-3 py-1 block ">
-              Customers
-            </Link>
+          <li className="">
+            <PageLink to="/regions">Regions</PageLink>
           </li>
         </ul>
       </nav>
     </header>
+  );
+};
+
+const PageLink = ({ to, children }) => {
+  const initialClassName =
+    "px-3 py-1 block transition-all border-b-4 font-bold text-gray-500 hover:text-black hover:border-green-700";
+  const activeClassName = "text-black border-green-700";
+  const inactiveClassName = "border-white";
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        `${initialClassName} ${isActive ? activeClassName : inactiveClassName}`
+      }
+    >
+      {children}
+    </NavLink>
   );
 };
