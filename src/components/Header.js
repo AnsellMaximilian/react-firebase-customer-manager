c9d887bc54535f0f727cf17b446f9784c58447d4
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-12-29 09:13:04 +0700

    add logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
 import logo from "../assets/images/logo.svg";
+import { signOut } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { SnackbarContext } from "../contexts/SnackbarContext";
 
 export const Header = () => {
+  const { openSnackbar } = useContext(SnackbarContext);
+
+  const handleSignOut = () => {
+    signOut(auth);
+    openSnackbar("Logged out successfully.", "success");
+  };
+
   return (
     <header className="py-2 shadow-md">
       <nav className="flex container mx-auto px-4">
         <img src={logo} alt="logo" width={100} />
-        <button className="ml-auto">Logout</button>
+        <button className="ml-auto" onClick={handleSignOut}>
+          Logout
+        </button>
       </nav>
     </header>
   );
