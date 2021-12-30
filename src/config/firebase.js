48c319225d85a4376e2566c2395cb321d4b09b5a
Ansell Maximilian <ansellmaximilian@gmail.com>
2021-12-30 13:32:01 +0700

    change to production

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-const development = true;
+const development = false;
 
 // Real config
 const realFirebaseConfig = {
