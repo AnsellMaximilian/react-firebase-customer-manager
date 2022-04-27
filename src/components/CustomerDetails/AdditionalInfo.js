0d901672cd7a59aa04d2ebf900c84719f569d701
Ansell Maximilian <ansellmaximilian@gmail.com>
2022-04-27 10:23:54 +0700

    add feedback when customer has no additional info

--- src/components/CustomerDetails/AdditionalInfo.js
+++ src/components/CustomerDetails/AdditionalInfo.js
@@ -5,12 +5,16 @@ const AdditionalInfo = ({ additionalInfo }) => {
     <div className="p-4 border-t border-gray-200">
       <div className="text-2xl font-bold mb-4">Additional Information</div>
       <div className="flex flex-col gap-2">
-        {Object.keys(additionalInfo).map((key) => (
-          <div key={key} className="">
-            <h4 className="text-xl font-semibold">{key}</h4>
-            <div>{additionalInfo[key]}</div>
-          </div>
-        ))}
+        {Object.keys(additionalInfo).length > 0 ? (
+          Object.keys(additionalInfo).map((key) => (
+            <div key={key} className="">
+              <h4 className="text-xl font-semibold">{key}</h4>
+              <div>{additionalInfo[key]}</div>
+            </div>
+          ))
+        ) : (
+          <div>No additional information. Edit customer to add.</div>
+        )}
       </div>
     </div>
   );
